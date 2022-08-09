caf12ca7b36a2d2b17063d8757a813486a5609f7
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-09 16:24:16 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,13 @@
+class  Variables{
+    static int c = 10;
+    int a=30;
+    public static void main(String[] args) {
+        Variables v=new Variables();
+        int b=20;
+        System.out.println(Variables.c);
+        System.out.println(b);
+        System.out.println(v.a);
+    }
+
+    
+}
