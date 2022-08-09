8bd305e8b28323c74046ac28a48f6833b981fb3a
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-09 22:30:41 +0530

    Add files via upload

---
+++ MultiplicationDivisionSubstractionAddition.java
@@ -0,0 +1,24 @@
+class MultiplicationDivisionSubstractionAddition{
+    public static void main(String[] args) {
+    System.out.println("the two numbers are:");
+        int a,b,c;
+        a=5;
+        b=6;
+        System.out.println(a+" "+b);
+        System.out.println("the result  of multiplication is:");
+        c=a*b;//logic of multiplication
+        System.out.println(c);
+        System.out.println("the result of division is:");
+        c=a/b;//logic of division
+        System.out.println(c);
+        System.out.println("the result of substraction is:");
+        c=a-b;//logic of substraction
+        System.out.println(c);
+        System.out.println("the result of addition is:");
+        c=a+b;//logic of addition
+        System.out.println(c);
+
+
+    }
+    
+}
