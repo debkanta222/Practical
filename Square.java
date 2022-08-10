21b4cb2c63d811825960189cbd3415c6bdaff93f
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-10 15:25:50 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,13 @@
+class Square{
+    public static void main(String[] args) {
+        int lengthOfSide =5;
+        int area;
+        int perimeter;
+        area=lengthOfSide*lengthOfSide;
+        System.out.println("the area is:" + area);
+        perimeter=4*lengthOfSide;
+        System.out.println("the perimeter is:" +perimeter);
+
+    }
+    
+}
