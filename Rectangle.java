39141afe908c0a190c070ed49159867b8468233a
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-10 15:26:23 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle{
+    public static void main(String[] args) {
+        int length,width,area,perimeter;
+        length=5;
+        width=6;
+        area=length*width;
+        System.out.println("the area is:"+ area);
+        perimeter=2*(length+width);
+        System.out.println("the perimeter is:" + perimeter);
+    }
+    
+}
