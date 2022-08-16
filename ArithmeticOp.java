1ae0db0c94155ae127abe7dde644ee58e683e785
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-16 16:46:58 +0530

    Add files via upload

---
+++ ArithmeticOp.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class ArithmeticOp {
+
+	public static void main(String[] args) {
+		float a=5f;
+		float b=3f;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
