e8055400bb7263a1aa066b73f3ff97a38d82396f
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-16 16:56:08 +0530

    Add files via upload

---
+++ LogicalOp.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class LogicalOp {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=10;
+		b=15;
+		System.out.println(a>b||b>a);//logical or operator
+		System.out.println(a<b&&b>a);//logical and operator
+		System.out.println(a>b&&b>a);//logical and operator
+		
+	}
+
+}
