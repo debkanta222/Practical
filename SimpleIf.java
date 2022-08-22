a7041ead65954ed8517168e274fcaa81cdf8ece0
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-22 14:11:16 +0530

    Add files via upload

---
+++ SimpleIf.java
@@ -0,0 +1,14 @@
+package Activity;
+
+public class SimpleIf {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		if(a+b>15) {
+			System.out.println("a+b is less than 15");
+		}
+
+	}
+
+}
