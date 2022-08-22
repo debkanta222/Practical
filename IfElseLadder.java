f5521eeb2cd158644e852f6c902ef063a904d04c
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-22 14:12:02 +0530

    Add files via upload

---
+++ IfElseLadder.java
@@ -0,0 +1,20 @@
+package Activity;
+
+public class IfElseLadder {
+
+	public static void main(String[] args) {
+		String name="abhi";
+		if(name=="rahul") {
+			System.out.println("rahul");
+		}
+		else if(name=="ram") {
+			System.out.println("ram");
+			
+		}
+		else {
+			System.out.println(name);
+		}
+
+	}
+
+}
