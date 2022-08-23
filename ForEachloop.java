53f31d863783eb400ff43fcb22240e0d3c16f24d
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-23 16:51:22 +0530

    Add files via upload

---
+++ ForEachloop.java
@@ -0,0 +1,13 @@
+package Activity;
+
+public class ForEachloop {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6};
+		for(int result :a) {
+			System.out.println(result);
+		}
+
+	}
+
+}
