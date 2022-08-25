963840227d1da84c65986c28932ebcff2b8f8aa3
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-08-25 17:46:56 +0530

    Add files via upload

---
+++ WhileLoopEg1.java
@@ -0,0 +1,18 @@
+package Activity;
+
+public class WhileLoopEg1 {
+
+	public static void main(String[] args) {
+		int i=1;
+		/*while(i<=5) {
+			System.out.println("execute");
+			i++;*/
+		while(true) {
+			System.out.println("execute");
+		}
+		}
+
+
+	}
+
+
