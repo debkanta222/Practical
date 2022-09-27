684e269d9406b2c6b48decc3ee242ec9133940df
Debkanta Chakraborty <110464926+debkanta222@users.noreply.github.com>
2022-09-27 20:41:34 +0530

    Add files via upload

---
+++ ClassNameArray.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ClassNameArray {
+public static void main(String[] args) {
+	int arr[]= {1,2,3};
+	Class c =arr.getClass();
+	String name =c.getName();
+	System.out.println(c);
+}
+}
